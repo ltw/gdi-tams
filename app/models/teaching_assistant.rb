285766c35921fff760b4ffe2f87cf258af9b14d7
Liz Abinante <eabinante@gmail.com>
2014-05-21 14:10:40 -0500

    remove hours from teaching assistants

--- app/models/teaching_assistant.rb
+++ app/models/teaching_assistant.rb
@@ -1,4 +1,8 @@
 class TeachingAssistant < ActiveRecord::Base
   belongs_to :status
   has_many :courses
+
+  scope :approved, -> { where status: Status.find_by_label("approved") }
+  scope :pending, -> { where status: Status.find_by_label("pending") }
+  scope :banned, -> { where status: Status.find_by_label("banned") }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521184844) do
+ActiveRecord::Schema.define(version: 20140521190258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140521184844) do
     t.integer  "status_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "hours"
   end
 
 end
