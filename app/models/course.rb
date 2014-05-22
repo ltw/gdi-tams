e54beb2c897b67db9efbc38b861bf0c0dd77160b
Liz Abinante <eabinante@gmail.com>
2014-05-22 08:52:13 -0500

    fixed hour scopes, add hour_for

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@ class Course < ActiveRecord::Base
   has_many :teaching_assistants, through: :hours
 
   scope :upcoming, lambda { where("date > ?", Date.today) }
+
+  def hour_for(teaching_assistant)
+    @hour = hours.where(teaching_assistant: teaching_assistant).first
+  end
 end
--- app/models/hour.rb
+++ app/models/hour.rb
@@ -2,6 +2,6 @@ class Hour < ActiveRecord::Base
   belongs_to :teaching_assistant
   belongs_to :course
 
-  scope :credit, -> { where(:num < 0) }
-  scope :debit, -> { where(:num > 1) }
+  scope :credit, -> { where("num > ?", 0) }
+  scope :debit, -> { where("num < ?", 1) }
 end
