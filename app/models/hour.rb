58bb35097c59746297fe2f177fa4013112bd02c5
Liz Abinante <eabinante@gmail.com>
2014-05-22 09:12:02 -0500

    hour rsvps work

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -4,4 +4,12 @@ class Hour < ActiveRecord::Base
 
   scope :credit, -> { where("num > ?", 0) }
   scope :debit, -> { where("num < ?", 1) }
+
+  def is_credit?
+    num > 0
+  end
+
+  def is_debit?
+    num < 0
+  end
 end
