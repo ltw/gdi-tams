d483d525ed178b4f82f164df6d21b1f293f60351
Liz Abinante <eabinante@gmail.com>
2014-05-22 08:51:50 -0500

    sign up show page

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -1,4 +1,5 @@
 class SignUpsController < ApplicationController
+  # Requires a TA - looks them up by private ID
   def show
     @teaching_assistant = TeachingAssistant.find_by_private_id(params[:private_id])
     @courses = Course.upcoming
