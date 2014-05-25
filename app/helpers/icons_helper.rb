7c8f3a7bd2464ae6b19bbbedfb14f360af3b7b1b
Liz Abinante <eabinante@gmail.com>
2014-05-25 10:32:25 -0500

    alphabetized

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -1,45 +1,45 @@
 module IconsHelper
-  def icon_link_out
-    raw('<i class="fa fa-external-link"></i>')
+  def icon_add
+    raw('<i class="fa fa-plus-circle"></i>')
   end
 
-  def icon_delete
-    raw('<i class="fa fa-minus-circle"></i>')
-  end
-
-  def icon_thumbs_up
-    raw('<i class="fa fa-thumbs-up"></i>')
+  def icon_book
+    raw('<i class="fa fa-book"></i>')
   end
 
   def icon_check
     raw('<i class="fa fa-check"></i>')
   end
 
-  def icon_add
-    raw('<i class="fa fa-plus-circle"></i>')
+  def icon_code
+    raw('<i class="fa fa-code"></i>')
   end
 
-  def icon_vertical_stack
-    raw('<i class="fa fa-bars"></i>')
+  def icon_delete
+    raw('<i class="fa fa-minus-circle"></i>')
   end
 
   def icon_edit
     raw('<i class="fa fa-pencil-square"></i>')
   end
 
-  def icon_code
-    raw('<i class="fa fa-code"></i>')
-  end
-
   def icon_history
     raw('<i class="fa fa-history"></i>')
   end
 
-  def icon_woman
-    raw('<i class="fa fa-female"></i>')
+  def icon_link_out
+    raw('<i class="fa fa-external-link"></i>')
   end
 
-  def icon_book
-    raw('<i class="fa fa-book"></i>')
+  def icon_thumbs_up
+    raw('<i class="fa fa-thumbs-up"></i>')
+  end
+
+  def icon_vertical_stack
+    raw('<i class="fa fa-bars"></i>')
+  end
+
+  def icon_woman
+    raw('<i class="fa fa-female"></i>')
   end
 end
