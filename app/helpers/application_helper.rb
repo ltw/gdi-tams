ca15c08efec52143dcedf2a59c92598d19c1be5b
Liz Abinante <eabinante@gmail.com>
2014-05-25 08:54:11 -0500

    extracted icons into helpers so i do not die from insanity

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
 module ApplicationHelper
+  include IconsHelper
 end
---
+++ app/helpers/icons_helper.rb
@@ -0,0 +1,13 @@
+module IconsHelper
+  def icon_link_out
+    raw('<i class="fa fa-external-link"></i>')
+  end
+
+  def icon_delete
+    raw('<i class="fa fa-minus-circle"></i>')
+  end
+
+  def icon_thumbs_up
+    raw('<i class="fa fa-thumbs-up"></i>')
+  end
+end
