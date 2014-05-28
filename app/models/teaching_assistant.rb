df85ea4f08d1f5f57178e32bd021f59163b96c6c
Liz Abinante <eabinante@gmail.com>
2014-05-28 18:23:09 -0500

    active class method for TAs, organized model file, separated methods into before_action in admins_controller

--- app/models/teaching_assistant.rb
+++ app/models/teaching_assistant.rb
@@ -8,25 +8,15 @@ class TeachingAssistant < ActiveRecord::Base
   validates_uniqueness_of :private_id, :email
   validates_presence_of :name, :email, :status
 
+
+  # STATUSES
   scope :approved, -> { where status: Status.find_by_label("approved") }
   scope :pending, -> { where status: Status.find_by_label("pending") }
   scope :banned, -> { where status: Status.find_by_label("banned") }
   scope :prospective, -> { where status: Status.find_by_label("prospective") }
 
-  def pending_balance
-    hours.to_a.map(&:num).inject(&:+)
-  end
-
-  def balance
-    history.to_a.map(&:num).inject(&:+)
-  end
-
-  def history
-    hours.select { |h| h.course.date < Date.today }.sort_by { |h| h.course.date }
-  end
-
-  def schedule
-    hours.where('num > 0').select { |h| h.course.date > Date.today }.map(&:course).sort_by(&:date)
+  def self.active
+    self.where.not(status: [Status.find_by_label("inactive"), Status.find_by_label("banned"), Status.find_by_label("prospective")])
   end
 
   def pending?
@@ -37,12 +27,32 @@ class TeachingAssistant < ActiveRecord::Base
     status.label == "approved"
   end
 
+  def inactive?
+    (status.label == "inactive") || (status.label == "banned")
+  end
+
   def prospective?
     status.label == "prospective"
   end
 
-  def inactive?
-    (status.label == "inactive") || (status.label == "banned")
+
+  # BALANCES
+  def pending_balance
+    hours.to_a.map(&:num).inject(&:+)
+  end
+
+  def balance
+    history.to_a.map(&:num).inject(&:+)
+  end
+
+
+  # SCHEDULING & ATTENDANCE
+  def history
+    hours.select { |h| h.course.date < Date.today }.sort_by { |h| h.course.date }
+  end
+
+  def schedule
+    hours.where('num > 0').select { |h| h.course.date > Date.today }.map(&:course).sort_by(&:date)
   end
 
   def signed_up_for(course)
@@ -63,6 +73,7 @@ class TeachingAssistant < ActiveRecord::Base
     hours.debit.where(course: course).present?
   end
 
+  private
   def generate_private_id
     self.private_id = SecureRandom.hex
   end
