03d174f9bf9c6ab1a64270b8f81a7f9932994053
Liz Abinante <eabinante@gmail.com>
2014-05-28 18:14:20 -0500

    remove admin params since they are not needed

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -11,15 +11,4 @@ class AdminsController < ApplicationController
   def emails
     render 'shared/admin_only' unless is_admin?
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_admin
-      @admin = Admin.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def admin_params
-      params.require(:admin).permit(:name, :password)
-    end
 end
