fb14da350786d1c6a9aa1daaeff9fee0b027fe6f
Liz Abinante <eabinante@gmail.com>
2014-05-29 18:57:12 -0500

    factory for admin users and commented out something i will use a lot

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,2 +1,13 @@
-module Factories
+FactoryGirl.define do
+  factory :admin do
+    name "liz"
+    password  "tacos"
+  end
+
+  # This will use the User class (Admin would have been guessed)
+  # factory :admin, class: User do
+  #   first_name "Admin"
+  #   last_name  "User"
+  #   admin      true
+  # end
 end
