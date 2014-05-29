dc8b8680e3ef1d0144fde14d3b0c9ec7b44470ba
Liz Abinante <eabinante@gmail.com>
2014-05-29 18:38:46 -0500

    status spec outline and presence validations added to model

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,5 @@
 class Status < ActiveRecord::Base
   has_many :teaching_assistants
+
+  validates_presence_of :label
 end
---
+++ spec/models/status_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Series do
+  context 'relationships' do
+    it 'has_many teaching_assistants'
+  end
+
+  context 'validations' do
+    it 'requires label'
+  end
+end
