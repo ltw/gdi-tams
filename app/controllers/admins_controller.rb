fb5d42d0207d9b1d2458605efb0d8b5eda28bc39
Liz Abinante <eabinante@gmail.com>
2014-05-30 16:14:46 -0500

    way less db calls

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,9 +2,7 @@ class AdminsController < ApplicationController
 
   def dashboard
     render 'shared/admin_only' unless is_admin?
-    @courses = Course.upcoming.single_day.sort_by(&:date)
-    @series = Series.upcoming.sort_by(&:end_date)
-    @tas = TeachingAssistant.all.sort_by(&:name)
+    @courses = Course.upcoming.includes(:series, :teaching_assistants).sort_by(&:date)
     @approved = Status.find_by_label("approved")
     @banned = Status.find_by_label("banned")
     @pending = Status.find_by_label("pending")
