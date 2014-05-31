bdca329e737e525ecb36fe56d5b8d7b12095697d
Liz Abinante <eabinante@gmail.com>
2014-05-31 17:06:13 -0500

    add email flag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524155941) do
+ActiveRecord::Schema.define(version: 20140531220503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140524155941) do
     t.integer  "series_id"
     t.time     "start_time"
     t.time     "end_time"
+    t.boolean  "email_sent",     default: false
   end
 
   create_table "hours", force: true do |t|
