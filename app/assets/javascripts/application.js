c722282ce495109c1a76c7004b16127687d67897
Liz Abinante <eabinante@gmail.com>
2014-05-30 20:50:30 -0500

    move javascript to app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,35 +15,17 @@
 //= require turbolinks
 //= require_tree .
 
-$(".js-vertical-tab-content").hide();
-$(".js-vertical-tab-content:first").show();
+$(document).ready(function(){
+  $('.js-menu-trigger').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
 
-/* if in tab mode */
-$(".js-vertical-tab").click(function(event) {
-  event.preventDefault();
-
-  $(".js-vertical-tab-content").hide();
-  var activeTab = $(this).attr("rel");
-  $("#"+activeTab).show();
-
-  $(".js-vertical-tab").removeClass("is-active");
-  $(this).addClass("is-active");
-
-  $(".js-vertical-tab-accordion-heading").removeClass("is-active");
-  $(".js-vertical-tab-accordion-heading[rel^='"+activeTab+"']").addClass("is-active");
+  $('.js-menu-screen').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
 });
 
-/* if in accordion mode */
-$(".js-vertical-tab-accordion-heading").click(function(event) {
-  event.preventDefault()
-
-  $(".js-vertical-tab-content").hide();
-  var accordion_activeTab = $(this).attr("rel");
-  $("#"+accordion_activeTab).show();
-
-  $(".js-vertical-tab-accordion-heading").removeClass("is-active");
-  $(this).addClass("is-active");
-
-  $(".js-vertical-tab").removeClass("is-active");
-  $(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
-});
