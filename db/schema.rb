fa1c2359aca51f6984837c119fc30253e8fc1f06
Liz Abinante <eabinante@gmail.com>
2014-05-31 17:54:24 -0500

    including pretty date and time formats in the db because i have to for mailers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531220503) do
+ActiveRecord::Schema.define(version: 20140531224626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20140531220503) do
     t.time     "start_time"
     t.time     "end_time"
     t.boolean  "email_sent",     default: false
+    t.string   "pretty_time"
+    t.string   "pretty_date"
   end
 
   create_table "hours", force: true do |t|
