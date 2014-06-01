69a7ecd73d6518366b5280d6c4c7d950b052d53c
Liz Abinante <eabinante@gmail.com>
2014-06-01 10:49:07 -0500

    split up factories into past and present course/series dates

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -6,8 +6,6 @@ FactoryGirl.define do
     Faker::Internet.email(Faker::Name.name + n.to_s)
   end
 
-  # hour with teaching_assistant association
-  # created before_validations to pass requirement
   factory :hour do
     num 2
     course
@@ -26,78 +24,6 @@ FactoryGirl.define do
     end
   end
 
-  # course
-  # created in the future
-  factory :course do
-    name            Faker::Commerce.department
-    url             Faker::Internet.url
-    location        'A Place in Chicago'
-    credit_hours    2
-    num_tas_needed  4
-    meetup_id       '12345'
-    start_time      2.hours.from_now
-    end_time        6.hours.from_now
-    sequence(:date, 10) { |n| n.days.from_now }
-
-    # course with 5 hours
-    # hour with teaching assistant
-    factory :course_with_hours do
-      ignore do
-        hours_count 5
-      end
-
-      after(:create) do |course, evaluator|
-        create_list(:hour, evaluator.hours_count, course: course)
-      end
-    end
-  end
-
-  factory :course_past, class: Course do
-    name            Faker::Commerce.department
-    url             Faker::Internet.url
-    location        'A Place in Chicago'
-    credit_hours    2
-    num_tas_needed  4
-    meetup_id       '12345'
-    start_time      2.hours.from_now
-    end_time        6.hours.from_now
-    sequence(:date, 10) { |n| n.days.ago }
-  end
-
-  # series
-  factory :series do
-    name      Faker::Commerce.color.capitalize
-    end_date  40.days.from_now
-
-    # series with courses
-    factory :series_with_courses do
-      ignore do
-        courses_count 4
-      end
-
-      after(:create) do |series, evaluator|
-        create_list(:course, evaluator.courses_count, series: series)
-      end
-    end
-  end
-
-  # series in the past
-  factory :series_past, class: Series do
-    name      Faker::Commerce.color.capitalize
-    end_date  9.days.ago
-
-    # series with courses
-    factory :series_past_with_courses do
-      ignore do
-        courses_count 4
-      end
-
-      after(:create) do |series, evaluator|
-        create_list(:course_past, evaluator.courses_count, series: series)
-      end
-    end
-  end
-
   factory :teaching_assistant do
     name      Faker::Name.first_name + Faker::Name.last_name
     email
@@ -107,11 +33,4 @@ FactoryGirl.define do
   factory :status do
     label   'approved'
   end
-
-  # This will use the User class (Admin would have been guessed)
-  # factory :admin, class: User do
-  #   first_name "Admin"
-  #   last_name  "User"
-  #   admin      true
-  # end
 end
