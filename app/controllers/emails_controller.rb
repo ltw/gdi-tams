67aa4d9fbc7b5ed83677b0c2093132729f578449
Liz Abinante <eabinante@gmail.com>
2014-06-01 15:31:36 -0500

    remove TA fetching; covered in before action

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -11,7 +11,6 @@ class EmailsController < ApplicationController
   end
 
   def monthly
-    @tas = TeachingAssistant.active
     month = Date.today.strftime("%B")
     courses = Course.upcoming.includes(:series).sort_by(&:date)
     @tas.each do |ta|
