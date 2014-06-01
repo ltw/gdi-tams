d2a541c6a40e33b89fc8e766656e4bdfe5843b58
Liz Abinante <eabinante@gmail.com>
2014-06-01 08:11:06 -0500

    before_validations specs

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,9 +1,48 @@
 require 'spec_helper'
 
+# course = Course.new({
+#         name: 'Course',
+#         date: Date.today,
+#         url: 'http://google.com',
+#         location: 'A Place in Chicago',
+#         num_tas_still_needed: 4,
+#         meetup_id: '12345',
+#         credit_hours: 2,
+#         start_time: 2.hours.from_now,
+#         end_time: 6.hours.from_now
+#         })
+
 describe Course do
-  context 'validations' do
-    it 'requires credit_hours'
-    it 'requires num_tas_needed'
+  context 'before_validation#format_pretty_dates' do
+    before do
+      @course = Course.new({
+        name: 'Course',
+        date: Date.today,
+        url: 'http://google.com',
+        location: 'A Place in Chicago',
+        credit_hours: 2,
+        num_tas_needed: 4,
+        meetup_id: '12345',
+        start_time: 2.hours.from_now,
+        end_time: 6.hours.from_now
+        })
+    end
+
+    it 'passes validations' do
+      expect(@course.save!).to be_true
+    end
+
+    it 'formats pretty_date' do
+      @course.save!
+      pretty_date = @course.date.strftime("%B %e, %Y (%A)")
+      expect(pretty_date).to eq(@course.pretty_date)
+    end
+
+    it 'formats pretty_time' do
+      @course.save!
+      pretty_time = "#{@course.start_time.strftime("%I:%M %p")}–#{@course.end_time.strftime("%I:%M %p")}"
+      expect(pretty_time).to eq(@course.pretty_time)
+    end
   end
 
   context 'relationships' do
