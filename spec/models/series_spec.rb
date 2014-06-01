915af400b2a98db609ac8a161cf81295e33cc581
Liz Abinante <eabinante@gmail.com>
2014-06-01 10:43:32 -0500

    series spec

--- spec/models/series_spec.rb
+++ spec/models/series_spec.rb
@@ -1,18 +1,46 @@
 require 'spec_helper'
 
 describe Series do
-  context 'validtions' do
-    it 'requires name'
-    it 'requires end_date'
+  context 'validations' do
+    it 'requires name' do
+      series = Series.new(end_date: 5.weeks.from_now)
+      expect { series.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'requires end_date' do
+      series = Series.new(name: 'Hello')
+      expect { series.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
   end
 
   context 'course relationships' do
-    it 'has_many'
-    it 'destroys dependent courses'
+    before do
+      @series = create(:series_with_courses)
+    end
+
+    it 'has_many' do
+      expect(@series.courses.length).to be > 1
+    end
+
+    it 'destroys dependent courses' do
+      course = @series.courses.first
+      @series.destroy
+      expect { course.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
   end
 
   context 'class methods' do
-    it '#upcoming'
-    it '#last_month'
+    before do
+      create_list(:series_with_courses, 5)
+      create_list(:series_past_with_courses, 10)
+    end
+
+    it '#upcoming' do
+      expect(Series.upcoming.length).to eq(5)
+    end
+
+    it '#last_month' do
+      expect(Series.last_month.length).to eq(10)
+    end
   end
 end
