875ddbc6bdef0ed7b5629c9acb8f5a32852885a7
Liz Abinante <eabinante@gmail.com>
2014-06-01 07:56:25 -0500

    admin validations and model tests

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,2 +1,15 @@
 require 'spec_helper'
 
+describe Admin do
+  context 'validations' do
+    it 'requires name' do
+      admin = Admin.new(password: "taco")
+      expect { admin.save! }.to raise_error("Validation failed: Name can't be blank")
+    end
+
+    it 'requires password' do
+      admin = Admin.new(name: "taco")
+      expect { admin.save! }.to raise_error("Validation failed: Password can't be blank, Password can't be blank")
+    end
+  end
+end
