ac8145e52e1158195c13300e0178335e4606bdbf
Liz Abinante <eabinante@gmail.com>
2014-06-08 18:40:05 -0500

    following this tutorial: http://pixelhandler.com/posts/scaffold-for-a-browser-app-built-with-emberjs-rails-emberdata

--- app/assets/javascripts/router.js
+++
@@ -1,9 +0,0 @@
-// For more information see: http://emberjs.com/guides/routing/
-
-// TeachingAssistants.Router.map(function() {
-//   // this.resource('posts');
-// });
-
-TeachingAssistants.Router.map(function() {
-  return this.resource('course', { path: '/courses' });
-});
