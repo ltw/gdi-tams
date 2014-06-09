d3e241e633662a453126e933018d05434409840c
Liz Abinante <eabinante@gmail.com>
2014-06-08 21:05:31 -0500

    showing all courses for now

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -2,6 +2,6 @@ class Api::CoursesController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Course.upcoming.sort_by(&:date)
+    render json: Course.all.sort_by(&:date)
   end
 end
