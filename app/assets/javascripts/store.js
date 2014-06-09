35a7f1d9d6a82d2b6ae20702e8a5aa3360066dc2
Liz Abinante <eabinante@gmail.com>
2014-06-08 21:08:22 -0500

    switched to js instead of coffeescript

---
+++ app/assets/javascripts/store.js
@@ -0,0 +1,11 @@
+App.ApplicationStore = DS.Store.extend();
+
+DS.RESTAdapter.reopen({
+  namespace: 'api'
+});
+
+App.ApplicationAdapter = DS.RESTAdapter.extend({
+  namespace: 'api',
+  configure: 'plurals',
+  course: 'courses'
+});
