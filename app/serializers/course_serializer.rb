3a841aaca30343f0afadb15b068fd335e379ed70
Liz Abinante <eabinante@gmail.com>
2014-06-08 21:06:13 -0500

    required settings for ember data

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,3 +1,5 @@
 class CourseSerializer < ActiveModel::Serializer
+  embed :ids, include: true
+
   attributes :id, :name, :date, :url, :location, :num_tas_needed, :credit_hours, :series_id, :start_time, :end_time, :email_sent, :pretty_time, :pretty_date
 end
