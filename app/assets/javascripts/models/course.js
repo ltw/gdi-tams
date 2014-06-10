49a5ee7e452bd73c8642a342e8cd556a440e8085
Liz Abinante <eabinante@gmail.com>
2014-06-10 18:42:56 -0500

    course attributes

--- app/assets/javascripts/models/course.js
+++ app/assets/javascripts/models/course.js
@@ -1,3 +1,13 @@
 App.Course = DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  date: DS.attr('date'),
+  url: DS.attr('string'),
+  location: DS.attr('string'),
+  num_tas_needed: DS.attr('number'),
+  credit_hours: DS.attr('number'),
+  start_time: DS.attr('date'),
+  end_time: DS.attr('date'),
+  email_sent: DS.attr('boolean'),
+  pretty_time: DS.attr('string'),
+  pretty_date: DS.attr('string')
 });
