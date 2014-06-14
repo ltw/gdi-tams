5c1f0257f621a2f1dd1be178f056027096619dd6
Liz Abinante <eabinante@gmail.com>
2014-06-14 11:29:30 -0500

    new course object

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,6 +3,7 @@ class AdminsController < ApplicationController
 
   def dashboard
     @title = "Organizer Dashboard"
+    @course = Course.new
     render 'shared/admin_only' unless is_admin?
     @past_courses = Course.last_month.includes(:teaching_assistants, :series).sort_by(&:date).reverse
   end
