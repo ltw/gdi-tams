af66eb8c4b11bd95173a473901bbf8af2c204160
Liz Abinante <eabinante@gmail.com>
2014-06-15 13:18:27 -0500

    email from name so it is not "liza", change redirect for signing up

--- app/controllers/teaching_assistants_controller.rb
+++ app/controllers/teaching_assistants_controller.rb
@@ -35,7 +35,7 @@ class TeachingAssistantsController < ApplicationController
 
     GdiMailer.pending(@teaching_assistant).deliver! if @teaching_assistant.save
 
-    if @teaching_assistant.save
+    if @teaching_assistant.save && is_admin?
       redirect_to admins_dashboard_path, notice: "TA #{@teaching_assistant.name} successfully added and marked as prospective. Remember to process their application!"
     else
       redirect_to thanks_teaching_assistant_path
