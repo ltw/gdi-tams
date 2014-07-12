25d433b13c5d5909708b75ecd62bd9d00da78097
Liz Abinante <eabinante@gmail.com>
2014-07-12 14:51:54 -0500

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531224626) do
+ActiveRecord::Schema.define(version: 20140712193101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140531224626) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
   create_table "courses", force: true do |t|
